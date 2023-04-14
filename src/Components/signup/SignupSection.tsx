86fca46ba7d5646f37e80905c718e15afc727b5f
DevJaiswal79 <devj8496@gmail.com>
2023-04-14 23:13:36 +0530

    Added page change functionality to signup page

--- src/Components/signup/SignupSection.tsx
+++ src/Components/signup/SignupSection.tsx
@@ -1,65 +1,66 @@
-import React, { useState } from 'react'
-import TextField from '../TextField'
-import Button from '../Button'
-import styled from "styled-components"
+import React, { useState } from "react";
+import TextField from "../TextField";
+import Button from "../Button";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Signin = styled.section`
-    display: flex;
-    align-items: center;
-    gap: 50px;
-    flex-direction: column;
-    // padding-bottom: rem;
-    .section1{ visibility: visible; position: relative;}
-    .section2{ visibility: hidden; position: absolute;}
-    section{
-      width: 100%;
-      display: flex;
-      align-items: center;
-      gap: 50px;
-      flex-direction: column;
-    }
-    h1{
-      margin: 5vh 0rem;
-      font-size: 3.5rem;
-    }
-    h2{
-    font-size:2.5rem;
-    }
-    a{
-      color: #000;
-      font-weight: 700;
-      text-decoration: none;
-    }
-`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+  flex-direction: column;
+  // padding-bottom: rem;
+  h1 {
+    margin: 5vh 0rem;
+    font-size: 3.5rem;
+  }
+  h2 {
+    font-size: 2.5rem;
+  }
+  a {
+    color: #000;
+    font-weight: 700;
+    text-decoration: none;
+  }
+`;
 
 function SignupSection() {
-  const [PageNo, setPageNo] = React.useState(true)
-  function changeVis(){
-    setPageNo(prevPageNo => !prevPageNo)    
+  const [Visibility, setVisibility] = React.useState(true);
+  function changeVis() {
+    if (Visibility) setVisibility(false);
+    else console.log("Server Verification sent")
   }
   return (
     <Signin>
-        <h1>FarmX</h1>
-        <h2>Sign Up</h2>
-        <section className='section1'>
-          <TextField name='Name' id='name' type='text' />
-          <TextField name='Email ID' id='mail' type='text' />
-          <TextField name='Password' id='pass' type='password' />
-          <TextField name='Confirm Password' id='name' type='password' />
-        </section>
-        <section className='section2'>
-          <TextField name='Area Pin Code' id='name' type='text' />
-          <TextField name='Phone Number' id='mail' type='text' />
-          <TextField name='Date Of Birth (DD/MM/YYY)' id='pass' type='password' />
-        </section>
-        <Button Text='Sign Up' onClick={() => {changeVis()}}/>
-        <span>
-          Already have an account?  
-          <a><Link to= '../login'>  Sign In.</Link></a>
-        </span>
+      <h1>FarmX</h1>
+      <h2>Sign Up</h2>
+
+      {Visibility && <TextField name="Name" id="name" type="text" />}
+      {Visibility && <TextField name="Email ID" id="mail" type="text" />}
+      {Visibility && <TextField name="Password" id="pass" type="password" />}
+      {Visibility && (
+        <TextField name="Confirm Password" id="pass2" type="password" />
+      )}
+      {!Visibility && <TextField name="Area Pin Code" id="pin" type="text" />}
+      {!Visibility && <TextField name="Phone Number" id="phone" type="text" />}
+      {!Visibility && (
+        <TextField name="Date Of Birth (DD/MM/YYY)" id="dob" type="password" />
+      )}
+
+      <Button
+        Text="Sign Up"
+        onClick={() => {
+          changeVis();
+        }}
+      />
+      <span>
+        Already have an account?
+        <a>
+          <Link to="../login"> Sign In.</Link>
+        </a>
+      </span>
     </Signin>
-  )
+  );
 }
 
-export default SignupSection
+export default SignupSection;
