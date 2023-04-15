0502f16fa3865771da370d760140e447f4140025
DevJaiswal79 <devj8496@gmail.com>
2023-04-15 19:05:42 +0530

    Added Object prop to HistorySection

--- src/Components/buyProfile/HistoryCard.tsx
+++ src/Components/buyProfile/HistoryCard.tsx
@@ -1,57 +1,63 @@
-import React from 'react'
-import styled from 'styled-components'
-import dummy from "/imagedummy.png"
+import React from "react";
+import styled from "styled-components";
 
 const Card = styled.div`
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: flex-start;
+  // padding: 0.5rem;
+  gap: 0.1rem;
+  border: 1px solid rgba(61, 61, 61, 0.3);
+  border-radius: 5px;
+  > .hist-card-container {
     width: 100%;
-    align-items: flex-start;
-    // padding: 0.5rem;
-    gap: 0.1rem;
-    border: 1px solid rgba(61, 61, 61, 0.3);
-    border-radius: 5px;
-    > .hist-card-container{
-        width: 100%;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-        padding: 0.5rem;
-        > img{
-            border-radius: 5px;
-            width:5.5rem
-        }
-        > .card-text{
-            width: 100%;
-            font-size: 1.2rem;
-            display: flex;
-            flex-direction: column;
-            >.row{
-                display: flex;
-                width: 100%;
-                justify-content: space-between;
-            }
-            
-        }
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 0.5rem;
+    > img {
+      border-radius: 5px;
+      width: 5.5rem;
     }
-`
+    > .card-text {
+      width: 100%;
+      font-size: 1.2rem;
+      display: flex;
+      flex-direction: column;
+      > .row {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+      }
+    }
+  }
+`;
 
-function HistoryCard() {
-  return (
-    <Card>
-        <div className='hist-card-container'>
-            <img src={dummy} alt="" />
-            <div className='card-text'>
-                <h3>Name</h3>
-                <div className='row'>
-                    <p>Stock : {2}</p>
-                    <p>Payment : {"₹ 200"}</p>
-                </div>
-                <p>Mandi : {"XYZ"}</p>
-            </div>
-        </div>
-    </Card>
-  )
+interface Props {
+  Name: string;
+  Stock: number;
+  Payment: number;
+  Mandi: string;
+  img: string;
 }
 
-export default HistoryCard
+function HistoryCard({ Name, Stock, Payment, Mandi, img }: Props) {
+  return (
+    <Card>
+      <div className="hist-card-container">
+        <img src={img} alt="" />
+        <div className="card-text">
+          <h3>{Name}</h3>
+          <div className="row">
+            <p>Stock : {Stock}</p>
+            <p>Payment : {"₹"+Payment}</p>
+          </div>
+          <p>Mandi : {Mandi}</p>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export default HistoryCard;
--- src/Components/buyProfile/HistorySection.tsx
+++ src/Components/buyProfile/HistorySection.tsx
@@ -1,31 +1,46 @@
-import React from 'react'
-import styled from 'styled-components'
-import HistoryCard from './HistoryCard'
+import React from "react";
+import styled from "styled-components";
+import HistoryCard from "./HistoryCard";
+import dummy from "/imagedummy.png";
 
 const HistSec = styled.div`
+width: 90%; 
+  > ul {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: start;
     gap: 1rem;
-    width: 80%;
     padding: 1rem;
     border: 1px solid rgba(61, 61, 61, 0.3);
     border-radius: 5px;
-`
-
+  }
+  li {
+  width: 100%; 
+    list-style: none;
+  }
+`;
 
 function HistorySection() {
+  const items = [
+    { id: 0, Name: "Name", Stock: 1, Mandi: "XYZ", Payment: 500, img: dummy },
+    { id: 1, Name: "Name", Stock: 2, Mandi: "XYZ", Payment: 600, img: dummy },
+    { id: 2, Name: "Name", Stock: 3, Mandi: "XYZ", Payment: 700, img: dummy },
+    { id: 7, Name: "Name", Stock: 4, Mandi: "XYZ", Payment: 800, img: dummy },
+    { id: 4, Name: "Name", Stock: 5, Mandi: "XYZ", Payment: 900, img: dummy },
+    { id: 5, Name: "Name", Stock: 6, Mandi: "XYZ", Payment: 1000, img: dummy },
+  ];
+  const history = items.map((item) => (
+    <li key={item.id}>
+      <HistoryCard {...item} />
+    </li>
+  ));
+
   return (
-    <HistSec >
-        <HistoryCard  />
-        <HistoryCard  />
-        <HistoryCard  />
-        <HistoryCard  />
-        <HistoryCard  />
-        <HistoryCard  />
+    <HistSec>
+      <ul>{history}</ul>
     </HistSec>
-  )
+  );
 }
 
-export default HistorySection
+export default HistorySection;
