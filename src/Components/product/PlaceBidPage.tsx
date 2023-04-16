48332cf698801f3061fecb8b016719fba6f3e689
DevJaiswal79 <devj8496@gmail.com>
2023-04-16 20:31:02 +0530

    Feature - Dynamically changing bid price

--- src/Components/product/PlaceBidPage.tsx
+++ src/Components/product/PlaceBidPage.tsx
@@ -1,17 +1,12 @@
-import React from 'react'
+import React, { useState } from "react";
 import styled from 'styled-components'
 import Navbar from '../Navbar';
 import { useLocation, useNavigate } from 'react-router-dom';
 import KeyValuePair from '../KeyValuePair';
 import Button from '../Button';
 
-// interface props {
-//     price: number;
-//     location: string;
-//     Date: string;
-// }
-
 const Container = styled.section`
+    min-height: calc( 100vh - 6.5rem);
     height: fit-content;
     margin-top: 5rem;
     width: 80%;
@@ -20,25 +15,40 @@ const Container = styled.section`
     align-items: center;
     justify-content: start;
     gap: 1rem;
-    #want-stock{
-        width: 80%;
-        font-size: 1.5rem;
-        margin: 1rem 0;
-    }
     .buttons{
         display: flex;
         gap: 1rem;
     }
-    #want-stock{
-        background-color: #fff;
-        width: fit-content;
-        border: none;
-        border-bottom: 1px solid #000;
-        padding: 5px;
-        margin: 0.2rem 0rem;
-        font-size: 1.25rem;
-        :focus{
-            outline: none;
+    .inputs{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: end;
+        justify-content: center;
+        padding: 1rem 0rem;
+        margin-bottom: auto;
+        > button{
+            padding: 0.5rem 1rem;
+        }
+        > div{
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+        }
+        input{
+            width: 80%;
+            font-size: 1.5rem;
+            margin: 1rem 0;
+            background-color: #fff;
+            width: fit-content;
+            border: none;
+            border-bottom: 1px solid #000;
+            padding: 5px;
+            margin: 0.2rem 0rem;
+            font-size: 1.25rem;
+            :focus{
+                outline: none;
+            }
         }
     }
 `
@@ -47,15 +57,54 @@ function PlaceBidPage() {
     const location = useLocation()
     const navigate = useNavigate()
     const {Name, Stock, Payment, address, Date} = location.state
+
+    const [bid , setBid] = useState({
+        stock: 0,
+        price: 0
+    });
+
+    const handleStockChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(parseInt(e.target.value) <= Stock && parseInt(e.target.value) > 0){
+            setBid({...bid, stock: parseInt(e.target.value)});
+        }
+        else{
+            setBid({...bid, stock: Stock});
+            if(parseInt(e.target.value) > Stock ){
+                alert("You can't buy more than available stock");
+                e.target.value = Stock.toString();
+            }
+            if(parseInt(e.target.value) <= 0 ){
+                alert("You can't buy less than 1 stock");
+                e.target.value = "1";
+            }
+        }
+    };
+    const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(parseInt(e.target.value) > 0 && parseInt(e.target.value) <= Payment)
+            setBid({...bid, price: parseInt(e.target.value)});
+        else{
+            setBid({...bid, price: Payment});
+            if(parseInt(e.target.value) <= 0 ){
+                alert("You can't bid less than 1");
+                e.target.value = "1";
+            }
+            if(parseInt(e.target.value) >= Payment ){
+                e.target.value = Payment.toString();
+                alert("You can't bid more than the Buying price");
+            }
+        }
+    };
+
     function placeBid(path: string) {
-        console.log("Bid To be Placed and Data to be read from the form")
         if (path === "-1") {
             navigate(-1)
         }
-        else navigate(path)
+        else {
+            console.log("Bid To be Placed and Data : ", bid) // Data to be read from the form
+            navigate(path)
+        }
     }
-    // const wantedStock = document.getElementById('want-stock').value;
-    // console.log(wantedStock)
+    
   return (
     <>
         <Navbar />
@@ -63,16 +112,19 @@ function PlaceBidPage() {
             <h1>{Name}</h1>
             <p>by Farmer's name</p>
             <hr />
-            <KeyValuePair Key='Amount to be paid' Value={"₹"+ (Payment * 1)} />
+            <KeyValuePair Key='Amount to be paid' Value={"₹"+ (bid.price * bid.stock )} />
             <hr />
             <KeyValuePair Key='Pickup Location' Value={address} />
             <hr />
             <KeyValuePair Key='Pickup Date' Value={Date} />
             <hr />
-            <input placeholder='Enter amount of stock' id='want-stock' type='number'/>
+            <div className='inputs'>
+                <input onChange={handlePriceChange} placeholder='Bid your Price' id='bid' type='number'/>
+                <input onChange={handleStockChange} placeholder='Enter amount of stock' id='want-stock' type='number'/>
+            </div>
             <div className='buttons'>
                 <Button Text="Cancel" onClick={() => {placeBid("-1")}} />
-                <Button Text="Place Bid" onClick={() => {placeBid("/home/buy/search/product/confirm")}} />
+                <Button Text="Submit" onClick={() => {placeBid("/home/buy/search/product/confirm")}} />
             </div>
         </Container>
     </>
